f7553c58a850389ff2f7ea10a8004efbc6a68430
Cheren <kalistratov.s.m@gmail.com>
2018-03-15 16:53:58 +0300

    Fix ./bin/app-img-area-select.sh + fix setup data in bootstrap.php

--- bin/app-img-area-select.sh
+++ bin/app-img-area-select.sh
@@ -25,9 +25,9 @@ fi
 
 if [ ! -f $FILE ]
     then
-        echo -e "\033[0;33m>>> >>> >>> >>> >>> >>> >>> >>> \033[0;30;46m Clone JQuery table check all \033[0m"
+        echo -e "\033[0;33m>>> >>> >>> >>> >>> >>> >>> >>> \033[0;30;46m Clone JQuery Image Area Select \033[0m"
         git clone --depth=50 --branch=master https://github.com/odyniec/imgareaselect.git $GITDIR/imgareaselect
 
     else
-        echo -e "\033[0;33m>>> >>> >>> >>> >>> >>> >>> >>> \033[0;30;46m JQuery table all ready exists \033[0m"
+        echo -e "\033[0;33m>>> >>> >>> >>> >>> >>> >>> >>> \033[0;30;46m JQuery Image Area Select all ready exists \033[0m"
     fi
